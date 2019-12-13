295d3d21256489a1c625d7fa39b446a104ac1e0c
Antoine Araujo <aaraujo@axome.com>
2019-12-13 08:48:26 +0100

    [*] AJout docsify

--- edit/index.php
+++ edit/index.php
@@ -1,20 +1,21 @@
-<?php $content = file_get_contents(__DIR__ . '/front/TEST.md'); ?>
+<?php $content = file_get_contents(__DIR__ . '/../README.md'); ?>
 
 <html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="dist/simplemde.min.css">
-        <script src="dist/simplemde.min.js"></script>
+ <head>
+        <link rel="stylesheet" type="text/css" href="../dist/simplemde.min.css">
+        <script src="../dist/simplemde.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
     </head>
 
 <body>
+    <div><a target="_blank" href="../">Voir le résultat</a></div>
     <textarea id="MyID"><?php echo $content; ?></textarea>
     <script>
         (function () {
             var simplemde = new SimpleMDE({element: document.getElementById("MyID")});
 
             simplemde.codemirror.on("change", function(){
-                writeTextFile('TEST.md', simplemde.value());
+                writeTextFile('README.md', simplemde.value());
                 console.log(simplemde.value());
             });
 
--- front/TEST.md
+++
@@ -1,8 +0,0 @@
-# TEST
-
-# TEST 2
-
-je test et c'est **ok**
-
-
-**TEST TEST**
